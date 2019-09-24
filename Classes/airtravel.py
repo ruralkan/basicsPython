1f90d1f12a52a41dc677e1b59af55e49f6708fee
ruralkan <ruralkan@gmail.com>
2019-09-24 14:00:20 -0500

    modification of Classes

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -1,18 +1,21 @@
 """Model for aircraft flights"""
 
 class Flight:
+    def __init__(self, number):
+        """" we use leading underscore by convention the implementation details of
+        objects which are not intended for consumtion or manipulation by clients
+        of the objects should be prefixed with underscore
+        """
+        self._number = number
+    
     def number(self):
-        return "SN060"
+        return self._number
 
 
 """
 In the REPL
 from airtravel import Flight
-g = Flight()
-g.number
-
-Try  too
-
-flight.number(g)
+g = Flight("SN060")
+g.number()
 """
 
