a63a6c9adb25704ea209c125d16ff967902c0285
ruralkan <ruralkan@gmail.com>
2019-09-24 14:24:51 -0500

    add other class

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -22,6 +22,25 @@ class Flight:
         return self._number[:2]
 
 
+
+class Aircraft:
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+        
+    def model(self):
+        return self._model
+        
+    def seating_plan(self):
+        return (range(1, self._num_rows + 1), 
+                "ABCDEFGHJK"[:self._num_seat_per_row])
+
+
 """
 In the REPL
 from airtravel import Flight
@@ -30,6 +49,5 @@ f = Flight("060")
 f = Flight("sn060")
 f = Flight("snabcd")
 f = Flight("sn12345")
-
 """
 
