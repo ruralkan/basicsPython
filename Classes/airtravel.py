a776a5c8239f6aa3ed8dea32d5e47ff9f764b874
ruralkan <ruralkan@gmail.com>
2019-09-24 14:07:21 -0500

    add validation and invariants

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -1,21 +1,35 @@
 """Model for aircraft flights"""
+""" Validation and invariants"""
 
 class Flight:
     def __init__(self, number):
-        """" we use leading underscore by convention the implementation details of
-        objects which are not intended for consumtion or manipulation by clients
-        of the objects should be prefixed with underscore
-        """
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+        
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code '{}'".format(number))
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("Invalid rout '{}'".format(number))
+
+
         self._number = number
     
     def number(self):
         return self._number
+
+    def airline(self):
+        return self._number[:2]
 
 
 """
 In the REPL
 from airtravel import Flight
 g = Flight("SN060")
-g.number()
+f = Flight("060")
+f = Flight("sn060")
+f = Flight("snabcd")
+f = Flight("sn12345")
+
 """
 
