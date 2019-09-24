2cd6a8be04ba1b811909fde0ae678e95932c51e8
ruralkan <ruralkan@gmail.com>
2019-09-24 13:52:06 -0500

    first Classes commit

---
+++ Classes/airtravel.py
@@ -0,0 +1,18 @@
+"""Model for aircraft flights"""
+
+class Flight:
+    def number(self):
+        return "SN060"
+
+
+"""
+In the REPL
+from airtravel import Flight
+g = Flight()
+g.number
+
+Try  too
+
+flight.number(g)
+"""
+
