325c90cbdec320a412c26284839d64bf7b38d51a
ruralkan <ruralkan@gmail.com>
2019-09-24 14:40:28 -0500

    add collaborating classes

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -1,8 +1,20 @@
 """Model for aircraft flights"""
-""" Validation and invariants"""
+""" Collaborating classes"""
+"""
+The law of Demeter is an object- oriented design principle that says you should never
+call methods on objcts you receive from other calls. O, put another way: Only talk to
+your immediate friend
+
+We'll now modify our Flight class to accept an aircraft object when it is constructed,
+and we'll follow the Law of Demeter by adding a method to report the aircraft model.
+This method delegate to Aircraft on bhelaf of the client rater than allowing the client 
+to "reach through" the Fligth and interrogate the Aircraft object directly.
+"""
 
 class Flight:
-    def __init__(self, number):
+    """A flight with a particular passenger aircraft"""
+
+    def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError("No airline code in '{}'".format(number))
         
@@ -12,14 +24,17 @@ class Flight:
         if not (number[2:].isdigit() and int(number[2:]) <= 9999):
             raise ValueError("Invalid rout '{}'".format(number))
 
-
         self._number = number
+        self._aircraft = aircraft
     
     def number(self):
         return self._number
 
     def airline(self):
         return self._number[:2]
+    
+    def aircraft_model(self):
+        return self._aircraft.model()
 
 
 
@@ -43,11 +58,9 @@ class Aircraft:
 
 """
 In the REPL
-from airtravel import Flight
-g = Flight("SN060")
-f = Flight("060")
-f = Flight("sn060")
-f = Flight("snabcd")
-f = Flight("sn12345")
+from airtravel import *
+f = Flight("BA758",Aircraft("G-EUPT", "Airbus A319", num_rows=22,
+            num_seats_per_row=6))
+f.aircraft()
 """
 
